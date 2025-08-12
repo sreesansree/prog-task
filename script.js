530778a49b5d9b2465fb589b0262e6eeb484570d
SREESANKAR-P <sreesankarputhumana@gmail.com>
2025-08-12 21:31:35 +0530

    Added Animation when reach the section

--- script.js
+++ script.js
@@ -11,4 +11,23 @@ document.getElementById("menu-icon").addEventListener("click", function () {
   icon.innerHTML = nav.classList.contains("mobile-menu-active")
     ? "&times;"
     : "&#9776;";
-}); 
+});
+
+const sections = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show"); // Trigger animation
+        observer.unobserve(entry.target); // Animate only once
+      }
+    });
+  },
+  {
+    threshold: 0.2, // Trigger when 20% of section is visible
+  }
+);
+sections.forEach((section) => {
+  observer.observe(section);
+});
